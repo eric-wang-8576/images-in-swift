b1802d9418751026dd56adba9e31ee9dddabd880
Eric Wang <ewang1@mit.edu>
2021-06-12 23:42:50 -0400

    first commit

---
+++ SwiftUIImagePicker/SwiftUIImagePickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIImagePickerApp.swift
+//  SwiftUIImagePicker
+//
+//  Created by Eric Wang on 6/12/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIImagePickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
